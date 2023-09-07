e0444e68b3c7a416e5248ddfa51f41de889f6736
christiangroothuis <62361628+christiangroothuis@users.noreply.github.com>
2023-09-07 16:47:32 +0200

    Add file structure to solve.py

--- solver/solve.py
+++ solver/solve.py
@@ -1,52 +1,55 @@
 ##
-# 2WF90 Algebra for Security -- Software Assignment 1 
+# 2WF90 Algebra for Security -- Software Assignment 1
 # Integer and Modular Arithmetic
 # solve.py
 #
 #
 # Group number:
-# group_number 
+# 21
 #
 # Author names and student IDs:
-# author_name_1 (author_student_ID_1) 
-# author_name_2 (author_student_ID_2)
-# author_name_3 (author_student_ID_3)
-# author_name_4 (author_student_ID_4)
+# Thijs Notten (author_student_ID_1)
+# Tom Nagel (author_student_ID_2)
+# Vincent Hoogendam (author_student_ID_3)
+# Christian Groothuis (1715534)
 ##
 
-# Import built-in json library for handling input/output 
+# Import built-in json library for handling input/output
 import json
 
 
 
-def solve_exercise(exercise_location : str, answer_location : str):
+def solve(exercise: object):
     """
     solves an exercise specified in the file located at exercise_location and
     writes the answer to a file at answer_location. Note: the file at
     answer_location might not exist yet and, hence, might still need to be created.
     """
-    
+    x = exercise["x"]
+    radix = exercise["radix"]
+    operation = exercise["operation"]
+
     # Open file at exercise_location for reading.
     with open(exercise_location, "r") as exercise_file:
-        # Deserialize JSON exercise data present in exercise_file to corresponding Python exercise data 
+        # Deserialize JSON exercise data present in exercise_file to corresponding Python exercise data
         exercise = json.load(exercise_file)
-        
+
 
     ### Parse and solve ###
 
     # Check type of exercise
     if exercise["type"] == "integer_arithmetic":
         # Check what operation within the integer arithmetic operations we need to solve
-        if exercise["operation"] == "addition":
+        if operation == "addition":
             # Solve integer arithmetic addition exercise
             pass
-        elif exercise["operation"] == "subtraction":
+        elif operation == "subtraction":
             # Solve integer arithmetic subtraction exercise
             pass
         # et cetera
     else: # exercise["type"] == "modular_arithmetic"
         # Check what operation within the modular arithmetic operations we need to solve
-        if exercise["operation"] == "reduction":
+        if operation == "reduction":
             # Solve modular arithmetic reduction exercise
             pass
         # et cetera
@@ -57,4 +60,21 @@ def solve_exercise(exercise_location : str, answer_location : str):
     with open(answer_location, "w") as answer_file:
         # Serialize Python answer data (stored in answer) to JSON answer data and write it to answer_file
         json.dump(answer, answer_file, indent=4)
-    
+
+
+def solve_from_file(exercise_location: str) -> object:
+    with open(exercise_location, "r") as exercise_file:
+        exercise = json.load(exercise_file)
+
+    return solve(exercise)
+
+
+def save_answer_to_file(answer_location: str, answer: object):
+    with open(answer_location, "w") as answer_file:
+        json.dump(answer, answer_file, indent=4)
+
+
+def solve_exercise(exercise_location: str, answer_location: str):
+    answer = solve_from_file(exercise_location)
+
+    save_answer_to_file(answer_location, answer)
