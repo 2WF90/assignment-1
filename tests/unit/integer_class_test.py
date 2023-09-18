2593f854ca346822783376f8265e574e146208ee
christiangroothuis <62361628+christiangroothuis@users.noreply.github.com>
2023-09-18 12:35:01 +0200

    Add tests for integer class from_string

---
+++ tests/unit/integer_class_test.py
@@ -0,0 +1,26 @@
+from src.helpers import *
+from src.integer import *
+
+
+def test_is_negative():
+    x = Integer.from_string("-AB894F")
+
+    assert True == x.is_negative
+
+
+def test_exponents_value():
+    x = Integer.from_string("-AB894F")
+
+    assert [15, 4, 9, 8, 11, 10] == x.exponents
+
+
+def test_to_string():
+    x = Integer.from_string("AB894F")
+
+    assert "AB894F" == x.to_string()
+
+
+def test_negative_to_string():
+    x = Integer.from_string("-AB894F")
+
+    assert "-AB894F" == x.to_string()
