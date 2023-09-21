b6c0c793b2e987fbdbf99c127ef677deae1e8fba
christiangroothuis <62361628+christiangroothuis@users.noreply.github.com>
2023-09-21 21:29:25 +0200

    Test if add works when both x, y are negative

--- tests/unit/add_test.py
+++ tests/unit/add_test.py
@@ -2,10 +2,18 @@ from src.basic_arithmetic import add, subtract
 from src.integer import Integer
 
 
-def test_is_negative2():
+def test_is_negative():
     x = Integer.from_string("10", 10)
     y = Integer.from_string("-9", 10)
 
     result = add(x, y)
 
     assert "1" == result.to_string()
+
+def test_x_and_y_negative():
+    x = Integer.from_string("-10", 10)
+    y = Integer.from_string("-9", 10)
+
+    result = add(x, y)
+
+    assert "-19" == result.to_string()
