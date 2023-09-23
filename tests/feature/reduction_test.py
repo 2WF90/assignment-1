7b5dfc060b4908b77d7c203a0130909f24afcebc
christiangroothuis <62361628+christiangroothuis@users.noreply.github.com>
2023-09-23 02:27:41 +0200

    Fix wrongly assigned tests

--- tests/feature/reduction_test.py
+++ tests/feature/reduction_test.py
@@ -2,11 +2,11 @@ from .assert_answer import assert_exercise
 
 
 def test_realistic_exercise_1():
-    assert_exercise("realistic", 1)
+    assert_exercise("realistic", 2)
 
 
 def test_realistic_exercise_2():
-    assert_exercise("realistic", 4)
+    assert_exercise("realistic", 5)
 
 
 def test_simple_exercise_1():
