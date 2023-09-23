e693aa54db62b190f24a13ccd32ab70b8d4c54d7
christiangroothuis <62361628+christiangroothuis@users.noreply.github.com>
2023-09-23 02:26:39 +0200

    Allow [0] exponent values

--- src/integer.py
+++ src/integer.py
@@ -22,8 +22,11 @@ class Integer:
         )
 
     def to_string(self) -> str:
+        if len(self.exponents) == 0:
+            return None
+
         return ("-" if self.is_negative else "") + "".join(
-            [get_representation(exponent) for exponent in self.exponents[::-1]]
+            [get_representation(exponent) for exponent in self.exponents[::-1]],
         )
 
     def pad(self, pad_length: int) -> Self:
@@ -32,10 +35,10 @@ class Integer:
         return self
 
     def strip_pad(self) -> Self:
-        while self.exponents and self.exponents[-1] == 0:
+        while len(self.exponents) > 1 and self.exponents[-1] == 0:
             self.exponents.pop()
 
-        return self
+        return Integer(self.exponents, self.is_negative, self.radix)
 
     def make_absolute(self) -> Self:
         self.is_negative = False
