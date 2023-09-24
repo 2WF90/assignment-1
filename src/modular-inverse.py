5112da001d56102c43b553b9c8130719084d7624
christiangroothuis <62361628+christiangroothuis@users.noreply.github.com>
2023-09-25 00:13:27 +0200

    Format code and return None when no solution

--- src/modular-inverse.py
+++ src/modular-inverse.py
@@ -1,23 +1,23 @@
-#import (subtraction, multiplication, devision)
 from division import divide, multiply
 from src.basic_arithmetic import add, subtract
+
 
 # Computes the modular inverse for a mod m. If it exists.
 def ModInverse(a: int, modulus: int):
     xOne = 1
     xTwo = 0
     while modulus > 0:
-        q, dump = divide(a,  m)
-        r= subtract(a, multiply(q, m))
+        q, _ = divide(a, m)
+        r = subtract(a, multiply(q, m))
         a = m
         m = r
         xThree = subtract(xOne, multiply(q, xTwo))
         xOne = xTwo
         xTwo = xThree
         pass
-    
+
     if a == 1:
         inverse = xOne
         return inverse
-    
-    return 'inverse does not exist'
+
+    return None
